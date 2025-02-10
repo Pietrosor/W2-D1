41a8b61d3b2f96d56deae67897bf04b5e9f27cd9
Pietrosor <pietrosorbo@hotmail.it>
2025-02-10 09:53:03 +0100

    First JS file

---
+++ script.js
@@ -0,0 +1,7 @@
+// SI PREFERISCE SCRIVERE JAVASCRIPT IN UN DIVERSO FILE
+// PER NON APPESANTIRE HTML E TENERLO PULITO
+
+// è collegato ad un file html con un tag <script>
+// all'invio della pagina html, funzionera anche questo in js
+
+console.log("Hello world");
